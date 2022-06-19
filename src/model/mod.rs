bb134fc935b67a56a470b0c64b8ffae4508884c6
Nertsal <sasha.kudasov04@gmail.com>
2022-06-19 16:02:57 +0300

    Triangular coordinate system and render

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,11 +1,107 @@
 use super::*;
 
 pub type Time = R32;
+pub type Turns = u64;
 
-pub struct Model {}
+pub struct Model {
+    pub player_a: Player,
+    pub player_b: Player,
+}
+
+/// A position in a triangular grid
+#[derive(Debug, Clone, Copy)]
+pub struct TriPos {
+    pub x: i64,
+    pub y: i64,
+}
+
+pub struct Player {
+    pub shape_buffer: ShapeBuffer,
+    pub farm: ShapeFarm,
+    pub active_shapes: ActiveShapes,
+}
+
+/// A shape is basically formed from cells in a triangular grid.
+pub struct Shape(pub Vec<TriPos>);
+
+pub struct ShapeBuffer {}
+
+pub struct ShapeFarm {}
+
+pub struct ActiveShapes {}
+
+pub struct Plant {
+    pub cooldown: Turns,
+    pub time_left: Turns,
+}
 
 impl Model {
     pub fn new() -> Self {
+        Self {
+            player_a: Player::new(),
+            player_b: Player::new(),
+        }
+    }
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self {
+            shape_buffer: ShapeBuffer::new(),
+            farm: ShapeFarm::new(),
+            active_shapes: ActiveShapes::new(),
+        }
+    }
+}
+
+impl ShapeBuffer {
+    pub fn new() -> Self {
         Self {}
+    }
+}
+
+impl ShapeFarm {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl ActiveShapes {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Plant {
+    pub fn tick(&mut self) -> bool {
+        if self.time_left <= 0 {
+            self.time_left = self.cooldown;
+            return true;
+        }
+        self.time_left -= 1;
+        false
+    }
+}
+
+impl TriPos {
+    pub fn to_cartesian(&self) -> Vec2<R32> {
+        let side = r32(1.0);
+        let root_3 = r32(3.0).sqrt();
+        vec2(
+            side * r32(0.5) * r32(self.x as _),
+            side * root_3 * r32(0.5) * r32(self.y as _)
+                + if self.is_upside_down() {
+                    r32(1.0)
+                } else {
+                    r32(-1.0)
+                } * side
+                    * root_3
+                    / r32(12.0),
+        )
+    }
+
+    /// Whether a triangle in that position points down
+    pub fn is_upside_down(&self) -> bool {
+        (self.x + self.y) % 2 != 0
     }
 }
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,5 +1,6 @@
 use super::*;
 
+use geng::{Camera2d, Draw2d};
 use model::*;
 
 pub struct Render {
@@ -15,5 +16,61 @@ impl Render {
         }
     }
 
-    pub fn draw(&mut self, _model: &Model, _framebuffer: &mut ugli::Framebuffer) {}
+    pub fn draw(&mut self, _model: &Model, framebuffer: &mut ugli::Framebuffer) {
+        let shape = Shape(vec![
+            TriPos { x: 0, y: 0 },
+            TriPos { x: 1, y: 0 },
+            TriPos { x: 0, y: 1 },
+            TriPos { x: -1, y: 0 },
+            TriPos { x: 0, y: -1 },
+            TriPos { x: 1, y: 1 },
+            TriPos { x: -1, y: 1 },
+            TriPos { x: 1, y: -1 },
+            TriPos { x: -1, y: -1 },
+        ]);
+        let camera = &Camera2d {
+            center: vec2(0.0, 0.0),
+            rotation: 0.0,
+            fov: 10.0,
+        };
+        self.draw_shape(&shape, camera, framebuffer);
+    }
+
+    pub fn draw_shape(
+        &self,
+        shape: &Shape,
+        camera: &Camera2d,
+        framebuffer: &mut ugli::Framebuffer,
+    ) {
+        for tri_pos in &shape.0 {
+            let angle = if tri_pos.is_upside_down() {
+                f32::PI
+            } else {
+                0.0
+            };
+            let pos = tri_pos.to_cartesian().map(|x| x.as_f32());
+            println!("Triangle at {tri_pos:?} (cartesian: {pos:?}) at angle {angle}");
+            self.draw_triangle(pos, angle, Color::GREEN, 0.7, camera, framebuffer)
+        }
+    }
+
+    pub fn draw_triangle(
+        &self,
+        pos: Vec2<f32>,
+        angle: f32,
+        color: Color<f32>,
+        side_length: f32,
+        camera: &Camera2d,
+        framebuffer: &mut ugli::Framebuffer,
+    ) {
+        // Calculate relative position for the triangle pointing up
+        let dx = side_length * 0.5;
+        let dy_low = side_length * 3.0.sqrt() / 6.0;
+        let dy_high = dy_low * 2.0;
+        let positions = [vec2(dx, -dy_low), vec2(-dx, -dy_low), vec2(0.0, dy_high)]
+            .into_iter()
+            .map(|position| position.rotate(angle) + pos)
+            .collect();
+        draw_2d::Polygon::new(dbg!(positions), color).draw_2d(&self.geng, framebuffer, camera);
+    }
 }
